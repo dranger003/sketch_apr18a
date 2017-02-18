ce94775b29a1a71f74073a749cc0bd8b1a9baf09
DAN™ <dranger003@iridium.localdomain>
2017-02-18 13:20:37 -0500

    AC phase control (dimmer).

--- sketch_apr18a.ino
+++ sketch_apr18a.ino
@@ -1,75 +1,96 @@
-#include <avr/io.h>
-#include <avr/interrupt.h>
+#define CLK1    _BV(CS10)               // 0.0625/4096uS
+#define CLK8    _BV(CS11)               // 0.5/32768uS
+#define CLK64   _BV(CS11) | _BV(CS10)   // 4/262144uS
+#define CLK256  _BV(CS12)               // 16/1048576uS
+#define CLK1024 _BV(CS12) | _BV(CS10)   // 64/4194304uS
 
-#define CLK1    (0x01 << CS10)                  // 0.0625/4096uS
-#define CLK8    (0x01 << CS11)                  // 0.5/32768uS
-#define CLK64   (0x01 << CS11) | (0x01 << CS10) // 4/262144uS
-#define CLK256  (0x01 << CS12)                  // 16/1048576uS
-#define CLK1024 (0x01 << CS12) | (0x01 << CS10) // 64/4194304uS
+#define HERTZ     60.0    // Hz
+#define PULSE     50.0    // ms
+#define DIM       50      // %
+#define CLKMULT   0.5     // CLK8
 
-volatile long t[] = { 0, 0, 0 };
-volatile unsigned short c[] = { 0, 0, 0 };
-volatile unsigned short d = 0;
+#define GATE      PB1     // PIN9
 
-unsigned short TIM16_ReadTCNT1() {
-  unsigned char sreg;
-  unsigned short v;
+const double _c = 1 / HERTZ / 2.0 * 1000000.0; // 8333.333...
+uint16_t _v = _c * DIM / 100.0 / CLKMULT + 0.5;
 
-  sreg = SREG;
-  cli();
-  v = TCNT1;
-  SREG = sreg;
-
-  return v;  
+ISR(_zc) {
+    TCNT1 = 0;
+    TCCR1B = CLK8;
 }
 
-void TIM16_WriteTCNT1(unsigned short v) {
-  unsigned char sreg;
-
-  sreg = SREG;
-  cli();
-  TCNT1 = v;
-  SREG = sreg;
+ISR(TIMER1_COMPA_vect) {
+  PORTB |= _BV(GATE);
+  TCNT1 = 65536 - (PULSE / CLKMULT + 0.5);
 }
+
+ISR(TIMER1_OVF_vect) {
+  PORTB &= ~_BV(GATE);
+  TCCR1B = 0x00;
+}
+
+// DDRx
+// 0=INPUT
+// 1=OUTPUT
+
+// PORTx
+// 0=LOW/
+// 1=HIGH/PULLUP
 
 void setup() {
   Serial.begin(115200);
   Serial.println("setup()");
 
-  TIMSK1 = (0x01 << OCIE1A) | (0x01 << TOIE1);
+  Serial.print("_v=");
+  Serial.println(_v);
 
+  DDRB = 0xFF;
+  PORTB = 0;
+
+  DDRC = 0xFF;
+  PORTC = 0;
+
+  DDRD = ~_BV(PD3); // PD3=INPUT
+  PORTD = 0;
+
+  attachInterrupt(1, _zc, RISING);
+
+  // TIMSK1   Timer/Counter1 Interrupt Mask Register
+  // OCIE1A   Timer/Counter1, Output Compare A Match Interrupt Enable
+  // TOIE1    Timer/Counter1, Overflow Interrupt Enable
+  TIMSK1 = _BV(OCIE1A) | _BV(TOIE1);
+
+  // TCCR1A   Timer/Counter1 Control Register A
+  // 0x00     Normal port operation, OC1A/OC1B disconnected.
+  // TCCR1B   Timer/Counter1 Control Register B
+  // 0x00     No clock source (Timer/Counter stopped)
   TCCR1A = 0x00;
   TCCR1B = 0x00;
-}
 
-ISR(zero) {
-  cli();
-  t[0] = micros();
-  TCCR1B = CLK64;
-  OCR1A = 65534;
+  // Timer/Counter (TCNT1)
   TCNT1 = 0;
-  sei();
-}
-
-ISR(TIMER1_COMPA_vect) {
-  c[0] = TIM16_ReadTCNT1();
-}
-
-ISR(TIMER1_OVF_vect) {
-  TCCR1B = 0x00;
-  d = 1;
+  // Output Compare Registers (OCR1A/B)
+  OCR1A = _v;
 }
 
 void loop() {
   if (Serial.available()) {
-    unsigned char c = Serial.read();
-    if (c == 'z' || c == 'Z')
-      zero();
-  }
+    auto p = Serial.readString().toDouble();
 
-  if (d == 1) {
-    Serial.println(c[0]);
-    d = 0;
+    Serial.print((uint32_t)p);
+    Serial.println("%");
+
+    p = 100 - p;
+    if (p == 0)
+      p = 1;
+    else if (p == 100)
+      p = 99;
+
+    uint16_t v = _c * p / 100.0 / CLKMULT + 0.5;
+
+    cli();
+    OCR1A = v;
+    sei();
   }
 }
 
